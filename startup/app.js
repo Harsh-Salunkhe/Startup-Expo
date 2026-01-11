aa3423aba540355e67867682bd029a67b6bc068a
Harsh-Salunkhe <harshsalunkhe120526@gmail.com>
2026-01-11 14:08:03 +0530

    some final changes

--- startup/app.js
+++ startup/app.js
@@ -71,19 +71,52 @@ window.addEventListener("scroll",NavEffect)
 
 // For Hamburber button and menu: 
 
+// const hamburgerButton = document.querySelector("#hamburger-menu");
+// const navBar = document.querySelector('nav')
+// const MobileMenu = document.querySelector("#mobile-menu");
+// let isMenuOpened = false;
+
+// const closeMenu = () => {
+//     window.addEventListener("scroll",NavEffect)
+//     hamburgerButton.children[0].classList.remove("cross-bar-1")
+//     hamburgerButton.children[1].style.opacity = '1'
+//     hamburgerButton.children[2].classList.remove("cross-bar-2")
+//     MobileMenu.style.transform = "translateX(-100%)"
+//     isMenuOpened = false;
+//     document.body.style.overflow = ""
+// }
+
+// const HamburgerButtonHandeler = () => {
+//     if(isMenuOpened){
+//         closeMenu();
+//     }
+//     else{
+//         window.removeEventListener("scroll",NavEffect)
+//         hamburgerButton.children[0].classList.add("cross-bar-1")
+//         hamburgerButton.children[1].style.opacity = '0'
+//         hamburgerButton.children[2].classList.add("cross-bar-2")
+//         MobileMenu.style.transform = "translateX(0)"
+//         isMenuOpened = true;
+//         // document.body.style.overflow = "hidden";
+//     }
+// }
+
+// hamburgerButton.addEventListener('click', HamburgerButtonHandeler);
+
+// Array.from(MobileMenu.children[0].children).forEach(li => li.addEventListener("click", closeMenu))
+// For Hamburger button and menu: 
+
 const hamburgerButton = document.querySelector("#hamburger-menu");
 const navBar = document.querySelector('nav')
 const MobileMenu = document.querySelector("#mobile-menu");
 let isMenuOpened = false;
 
 const closeMenu = () => {
-    window.addEventListener("scroll",NavEffect)
-    hamburgerButton.children[0].classList.remove("cross-bar-1")
-    hamburgerButton.children[1].style.opacity = '1'
-    hamburgerButton.children[2].classList.remove("cross-bar-2")
-    MobileMenu.style.transform = "translateX(-100%)"
+    window.addEventListener("scroll", NavEffect);
+    hamburgerButton.classList.remove("active");
+    MobileMenu.classList.remove("active");
     isMenuOpened = false;
-    document.body.style.overflow = ""
+    document.body.style.overflow = "";
 }
 
 const HamburgerButtonHandeler = () => {
@@ -91,20 +124,16 @@ const HamburgerButtonHandeler = () => {
         closeMenu();
     }
     else{
-        window.removeEventListener("scroll",NavEffect)
-        hamburgerButton.children[0].classList.add("cross-bar-1")
-        hamburgerButton.children[1].style.opacity = '0'
-        hamburgerButton.children[2].classList.add("cross-bar-2")
-        MobileMenu.style.transform = "translateX(0)"
+        window.removeEventListener("scroll", NavEffect);
+        hamburgerButton.classList.add("active");
+        MobileMenu.classList.add("active");
         isMenuOpened = true;
-        // document.body.style.overflow = "hidden";
     }
 }
 
 hamburgerButton.addEventListener('click', HamburgerButtonHandeler);
 
-Array.from(MobileMenu.children[0].children).forEach(li => li.addEventListener("click", closeMenu))
-
+Array.from(MobileMenu.children[0].children).forEach(li => li.addEventListener("click", closeMenu));
 // programming for count-down timer
 
 const   MainCountdownBox = document.getElementById("countdown")
